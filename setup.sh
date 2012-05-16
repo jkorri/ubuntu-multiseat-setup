4beca4fd56c02931b520e84c6059755c9075c95a
Your Name <santeri.korri@iki.fi>
2012-05-16 22:18:22 +0300

    Added installing chrome

--- setup.sh
+++ setup.sh
@@ -3,6 +3,15 @@
 #Install Flash
 sudo apt-get install flashplugin-installer
 
+#Install Chrome
+mkdir -p ./tmp
+CHROME_PACKAGE=google-chrome-stable_current_amd64.deb
+if [ ! -f ./tmp/$CHROME_PACKAGE ]
+then
+  wget https://dl.google.com/linux/direct/$CHROME_PACKAGE -O ./tmp/$CHROME_PACKAGE
+fi
+sudo dpkg -i ./tmp/$CHROME_PACKAGE
+
 #Setup multiseat
 sudo mv -f /etc/X11/xorg.conf /etc/X11/xorg.conf.backup
 sudo cp -f ./xorg.conf /etc/X11/xorg.conf
