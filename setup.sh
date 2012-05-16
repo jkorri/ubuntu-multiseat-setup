3c2f9d7d6ad72a7dd252b7db1f147aa32d83b1ad
Santeri Korri <santeri.korri@iki.fi>
2012-05-16 23:04:53 +0300

    Added nvidia and git setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+#Install nvidia driver
+sudo apt-get install nvidia-current
+
 #Install Flash
 sudo apt-get install flashplugin-installer
+
+#Install git
+sudo apt-get install  git
 
 #Install Chrome
 mkdir -p ./tmp
@@ -11,9 +17,16 @@ then
   wget https://dl.google.com/linux/direct/$CHROME_PACKAGE -O ./tmp/$CHROME_PACKAGE
 fi
 sudo dpkg -i ./tmp/$CHROME_PACKAGE
+sudo apt-get -f install
+sudo dpkg -i ./tmp/$CHROME_PACKAGE
+
 
 #Setup multiseat
-sudo mv -f /etc/X11/xorg.conf /etc/X11/xorg.conf.backup
+XORG_CONF=/etc/X11/xorg.conf
+if [ -f $XORG_CONF ]
+then
+  sudo mv -f $XORG_CONF $XORG_CONF.backup
+fi
 sudo cp -f ./xorg.conf /etc/X11/xorg.conf
 
 sudo mv -f /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.backup
