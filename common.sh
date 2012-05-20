b24ed310b056cc53705e700d7c85ff670a6a21a7
Santeri Korri <santeri.korri@iki.fi>
2012-05-20 12:17:54 +0300

    Fixed set_symlink case where link exists but points to wrong target

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@
 # Execute something for each user
 for_each_user() {
   for USERNAME in `cat /etc/passwd | grep -E "x:[0-9]{4}:" | cut -d":" -f1`; do
-    eval ${@//\{\}/$USERNAME}
+    ${@//\{\}/$USERNAME}
   done
 }
 
@@ -13,10 +13,14 @@ USERNAME=$1
 LINK_TARGET=$2
 LINK_NAME=$3
 
-if [ -f $LINK_NAME ] && [ `ls -l $LINK_NAME | cut -d">" -f2` = $LINK_TARGET ];
+if [ -f $LINK_NAME ]
 then
-  echo Link target for $LINK_NAME already set to $LINK_TARGET for user $USERNAME
-  return
+  if  [ `ls -l $LINK_NAME | cut -d">" -f2` = $LINK_TARGET ]
+  then
+    echo Link target for $LINK_NAME already set to $LINK_TARGET for user $USERNAME
+    return
+  fi
+  sudo rm -f $LINK_NAME
 fi
 
 sudo -u $1 mkdir -p `dirname $LINK_NAME`
