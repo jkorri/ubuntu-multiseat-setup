4bed971ee2749937f5791d3f73ca7914765ab5f3
Santeri Korri <santeri.korri@iki.fi>
2012-05-21 21:21:52 +0300

    Added creating symlinks to home folders to utilize data disk

--- data.sh
+++ data.sh
@@ -13,12 +13,23 @@ grep -q -E ^$MEDIA_USER: /etc/passwd
 if [ ! $? -eq 0 ];
 then
   echo creating user $MEDIA_USER
-  sudo adduser --no-create-home --disabled-login --quiet $MEDIA_USER
+  sudo adduser --system --group --quiet $MEDIA_USER
 fi
 sudo mkdir -p /data/media
 sudo chown media:media /data/media
-
 # s flag is crucial so that this functions as a shared folder
 sudo chmod ug+rwxs /data/media
 
 for_each_user sudo usermod -a -G $MEDIA_USER {}
+for_each_user set_symlink {} /data/media /home/{}/media
+
+# Setup parallel home folder
+sudo mkdir -p /data/home
+for_each_user sudo mkdir -p /data/home/{}
+for_each_user sudo chown {}:{} /data/home/{}
+
+for_each_user sudo -u {} mkdir -p /data/home/{}/Downloads
+for_each_user set_symlink {} /data/home/{}/Downloads /home/{}/Downloads
+
+for_each_user sudo -u {} mkdir -p /data/home/{}/Documents
+for_each_user set_symlink {} /data/home/{}/Documents /home/{}/Documents
