7aea0b5d563e13924344f6cd933f04dc6426af01
jkorri <santeri.korri@iki.fi>
2012-09-06 20:56:57 +0300

    Updated update-alternatives priority for java to dominate OpenJDK

--- java.sh
+++ java.sh
@@ -9,20 +9,22 @@ install_java() {
 #Install java, assumes download present in ./tmp
 JAVA_PACKAGE=jdk-7u5-linux-x64.tar.gz
 
-if [ ! -f ./tmp/$JAVA_PACKAGE ]
-then
-  echo Please download java package to tmp folder!
-  exit
-fi
-sudo mkdir -p /usr/lib/jvm/
-sudo mv ./tmp/$JAVA_PACKAGE /usr/lib/jvm/
-cd /usr/lib/jvm/
-sudo tar zxvf $JAVA_PACKAGE
+#if [ ! -f ./tmp/$JAVA_PACKAGE ]
+#then
+#  echo Please download java package to tmp folder!
+#  exit
+#fi
+#sudo mkdir -p /usr/lib/jvm/
+#sudo mv ./tmp/$JAVA_PACKAGE /usr/lib/jvm/
+#cd /usr/lib/jvm/
+#sudo tar zxvf $JAVA_PACKAGE
 
-sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/$JDK_DIR/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/$JDK_DIR/bin/javac" 1
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/$JDK_DIR/bin/javaws" 1
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/$JDK_DIR/bin/java" 1062
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/$JDK_DIR/bin/javac" 1062
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/$JDK_DIR/bin/javaws" 1062
 }
+
+install_java
 
 #Enable chrome/mozilla java plugin for all users
 for_each_user set_symlink {} "/usr/lib/jvm/$JDK_DIR/jre/lib/amd64/libnpjp2.so" /home/{}/.mozilla/plugins/libnpjp2.so
